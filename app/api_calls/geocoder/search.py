288fc079a2aac57a0fc4f77b3bc241b624b9df4c
Kyle <kyle@pop-os.localdomain>
2025-02-04 09:58:56 -0800

    update notes and type hints for geocoder

--- app/api_calls/geocoder/search.py
+++ app/api_calls/geocoder/search.py
@@ -1,18 +1,22 @@
-#The Geocoder API accepts both structured and free-form addresses as an input and returns JSON, GeoJSON, and XML objects as a response. In addition, you can specify location filters and preferred geographical areas to make the address search more accurate and focused
-
 import requests
 from requests.structures import CaseInsensitiveDict
-import json
 import os
 
-def geocode_address(address) -> tuple:
+def geocode_address(address:str) -> tuple:
     """
-    Accepts a free form typed street address and returns two variables, lat and lon
+    Gets the lat and long coordinates for a given address.
+
+    Params:
+    address (str): free form address such as a street address, city name, postal code, etc.
+
+    Returns:
+    - a tuple of the lat and long coordinates
     """
 
+    # format street address for url
     address_formatted = address.replace(" ", "%20")
 
-
+    # endpoint
     url = f"https://api.geoapify.com/v1/geocode/search?text={address_formatted}&apiKey={os.environ["GEOAPIFY_KEY"]}"
 
     headers = CaseInsensitiveDict()
